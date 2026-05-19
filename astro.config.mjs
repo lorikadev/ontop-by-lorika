96fd23d45dc7d1305ed90eee89c417853035af65
Dexaf <federicopisajob@gmail.com>
2026-05-19 11:51:14 +0200

    FEAT: added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://www.lorikacover.com',
   integrations: [
     sanity({
       projectId: "ta37clzz",
@@ -12,5 +13,6 @@ export default defineConfig({
       apiVersion: '2026-04-02',
       useCdn: false, // for static builds
     }),
+    sitemap()
   ]
 });
