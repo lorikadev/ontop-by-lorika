d9f4c1f08802ebf76127d2b7e6a887bf839cec4f
Dexaf <federicopisajob@gmail.com>
2026-05-21 18:33:06 +0200

    FEAT: added server function to handle redirect to shop

---
+++ netlify/functions/redirect-shop.ts
@@ -0,0 +1,26 @@
+import { SHOPIFY_URL } from "../../src/const";
+import type { Handler } from "@netlify/functions";
+
+export const handler: Handler = async (event) => {
+    const country = event.headers["x-nf-country"] || "";
+
+    const shopifyCountries = new Set([
+        "US", "CA",
+        "IT", "FR", "DE", "ES", "NL", "BE", "PT", "AT", "IE", "FI", "SE", "DK",
+        "NO", "CH", "PL", "CZ", "SK", "HU", "RO", "BG", "GR", "HR", "SI", "EE",
+        "LV", "LT", "LU", "MT", "CY", "IS", "LI", "UA", "GB"
+    ]);
+
+    const amazonUrl = "https://www.amazon.com";
+
+    const target = shopifyCountries.has(country)
+        ? SHOPIFY_URL
+        : amazonUrl;
+
+    return {
+        statusCode: 302,
+        headers: {
+            Location: target
+        }
+    };
+};
--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,3 @@
 export const HIGHLIGHT_COLOR_CSS_VAR_KEY = "--highlight-color";
 export const SHOPIFY_URL = "https://lorika-8398.myshopify.com/en";
+export const SHOP_REDIRECT_URL = "/.netlify/functions/redirect-shop";
