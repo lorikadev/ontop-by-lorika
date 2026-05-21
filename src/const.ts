53f57250ec11a362446c8d25c581c9a54c2f1685
Dexaf <federicopisajob@gmail.com>
2026-05-21 17:41:24 +0200

    FEAT: updated link to shop

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,2 @@
 export const HIGHLIGHT_COLOR_CSS_VAR_KEY = "--highlight-color";
-export const SHOPIFY_URL = "https://lorika-8398.myshopify.com/products/ontop-preorder";
+export const SHOPIFY_URL = "https://lorika-8398.myshopify.com/en";
